8c025a0c5cd41d8dfe4f23ee21212212ee1cbe20
ndooge@student.wethinkcode.co.za <ndooge@student.wethinkcode.co.za>
2020-07-14 03:39:57 -0400

    rush01:

---
+++ ex00/rush01.c
@@ -0,0 +1,29 @@
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int w;
+	int h;
+
+	w = x;
+	h = y;
+	y = 1;
+	while (y <= h)
+	{ 
+		x = 1;
+		while (x <= w)
+		{ 
+			if ((x < 2 && y < 2) || (x == w && y == h && y > 1 && x > 1))
+				ft_putchar('/'); 
+			else if (x > 1 && x < w && y > 1 && y < h)
+				ft_putchar(' ');
+			else if ((x == 1 && y == h) || (x == w && y == 1))
+				ft_putchar('\\');
+			else 
+				ft_putchar('*');
+			x++;
+		}
+		ft_putchar('\n');
+		y++;
+	}
+}
